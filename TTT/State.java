e8ba1cbbe6973101b4f0d8c9d695726cc788abb1
najlahna <najlahnaa@gmail.com>
2025-06-11 10:30:29 +0700

    First push

---
+++ TTT/State.java
@@ -0,0 +1,8 @@
+package TTT;
+
+/**
+ *  The enum TTT.State defines the various game states of the TTT game
+ */
+public enum State {  // to save as "TTT.State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
---
+++ TTTGUI/State.java
@@ -0,0 +1,8 @@
+package TTTGUI;
+
+/**
+ *  The enum TTT.State contains the various game states of the TTT game
+ */
+public enum State {  // to save as "TTT.State.java"
+    PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+}
